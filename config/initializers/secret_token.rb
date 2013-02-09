d6a084633158c8353d8e2b27b81893a094a97259
Pieter Gunst <pietergunst@gmail.com>
2013-02-09 11:20:02 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Incbot::Application.config.secret_token = 'b095262aafa8a8e15d731a5680a8a73518aa657e9c4daaa93f45aab9786c5c71c5ca7439aab6eac17f207f9c75a2e2ba3a1308dfc64fec93ef138bb18d8479a3'
