0a99f7decba6ed16cd6b76a49a6b45f8a8abaeaf
Pieter Gunst <pietergunst@gmail.com>
2013-02-10 02:22:19 -0800

    added form validations

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -1,5 +1,7 @@
 class Corporation < ActiveRecord::Base
   attr_accessible :filing_date, :name, :par_value, :shares
-  validates_presence_of :filing_date, :name, :par_value, :shares
-
+  validates :name, :presence => true, :uniqueness => true
+  validates :filing_date, :presence => true
+  validates :par_value, :presence => true, :format => {:with => /^[-.,0-9]+$/}
+  validates :shares, :presence => true, :format => {:with => /^[-.,0-9]+$/}
 end
