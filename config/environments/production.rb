a6710ec819979e878d052fd98b724a7df41a8081
Pieter Gunst <pietergunst@gmail.com>
2013-02-11 20:57:54 -0800

    updating for devise on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Incbot::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Devise requires defined default url options in your environments files
+  config.action_mailer.default_url_options = { :host => 'http://blooming-stream-1564.herokuapp.com' }
 end
